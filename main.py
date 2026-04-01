29f3d32f0f742ef405f3d541cd8983bde1470067
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-01 14:21:33 +0200

    ajout méthode ajouter une compétence

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 
 class Utilisateur:
+    # Constructeur
     def __init__(self, name, id):
         self.name = name
         self.id = id
@@ -16,21 +17,34 @@ class Apprenant(Utilisateur):
     # Constructeur
     def __init__(self, name, id, competences_validees):
         super().__init__(name, id)
-        self.competences_validees = competences_validees
+        self._competences_validees = competences_validees
 
     def peut_valider(self, competence_id: int) -> bool:
-        if competence_id in self.competences_validees:
+        if competence_id in self._competences_validees:
             return True
         else:
             return False
 
+    def ajouter_competence(self, competence_id: int):
+        if competence_id in self._competences_validees:
+            raise ValueError("La compétence exist déjà")
+        else:
+            self._competences_validees.append(competence_id)
+
+
 
 class Formateur(Utilisateur):
+
     def peut_valider(self, competence_id: int) -> bool:
         return True
 
 
+
 if __name__ == "__main__":
-    apprenant_1 = Apprenant("Bidule", 1, 1)
-    print(apprenant_1.name)
-    print(apprenant_1.id)
+    apprenant_1 = Apprenant("Bidule", 1, [1,2,3])
+    print(apprenant_1.name , apprenant_1.id, apprenant_1.peut_valider(1))
+    apprenant_1.ajouter_competence(4)
+    apprenant_1.ajouter_competence(1)
+
+
+
