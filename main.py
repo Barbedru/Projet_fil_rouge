b2a7829523bc92d1a326ee0ffe19e8d0dcbc5cc0
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-01 10:40:51 +0200

    ajout methode

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ class utilisateur:
         self.name = name
         self.age = age
 
+def peut_valider(competence_id: int) -> bool:
+    return false
 
 
 
-if __name__ == '__main__':
+
+
+
+#if __name__ == '__main__':
 
 
 
