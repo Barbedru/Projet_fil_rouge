9e987ac96d47d771389a746169f78ef8922205ef
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-01 10:54:15 +0200

    ajout class heritage

--- main.py
+++ main.py
@@ -4,11 +4,16 @@
 class Utilisateur:
     def __init__(self, name, age):
         self.name = name
-        self.age = age
+        self.id = id
 
     def utilisateur_1(self):
-        return f"{self.name} is {self.age} ans"
+        return f"{self.name} is {self.id} "
 
+
+class Apprenant(Utilisateur):
+
+
+class Formateur(Utilisateur):
 
 
 def peut_valider(competence_id: int) -> bool:
