f60167b5a9d854c346bc7e917c12a2f19d96c27d
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-02 16:37:43 +0200

    Ajout list formateur

--- src/utilisateurs/Formateur.py
+++ src/utilisateurs/Formateur.py
@@ -5,3 +5,9 @@ class Formateur(Utilisateur):
 
     def peut_valider(self, competence_id: int) -> bool:
         return True
+
+formateurs = [
+    Formateur ("f1",1),
+    Formateur ("f2",2),
+    Formateur ("f3",3),
+    ]
