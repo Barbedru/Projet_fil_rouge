42da094598348f4c8d1593ecdf9f54b736185917
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-02 16:38:05 +0200

    ras

--- src/main.py
+++ src/main.py
@@ -1,43 +1,34 @@
 from dataclasses import dataclass
 
 
-
 @dataclass
 class Competence:
-    id : int
-    nom : str
+    id: int
+    nom: str
+
 
 competences = [
-    Competence(1,"Python"),
-    Competence(2,"JavaScript"),
-    Competence(3,"Html"),
-    Competence(4,"Java"),
-    Competence(5,"PHP"),
-    Competence(6,"C#"),
-    Competence(7,"SQL"),
-    Competence(8,"Laravel"),
-    Competence(9,"JavaScript"),
-    Competence(10,"Css"),
+    Competence(1, "Python"),
+    Competence(2, "JavaScript"),
+    Competence(3, "Html"),
+    Competence(4, "Java"),
+    Competence(5, "PHP"),
+    Competence(6, "C#"),
+    Competence(7, "SQL"),
+    Competence(8, "Laravel"),
+    Competence(9, "JavaScript"),
+    Competence(10, "Css"),
 ]
 
 
 @dataclass
 class Validation:
-    id : int
-    apprenant_id : int
-    competence_id : int
-    statut : str
-    pre_valid_par : bool
-
-
-
+    id: int
+    apprenant_id: int
+    competence_id: int
+    statut: str
+    pre_valid_par: bool
 
 
 if __name__ == "__main__":
-   print(competences)
-
-
-
-
-
-
+    print(competences)
